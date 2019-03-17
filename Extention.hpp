78578f38e3083164bc9a2b7234ef72fe11c68624
MichaelFed <47292867+MichaelFed@users.noreply.github.com>
2019-03-17 12:47:03 +0300

    Add files via upload

---
+++ Extention.hpp
@@ -0,0 +1,10 @@
+#include <random>
+namespace ext
+{
+	int GetRandomValue(int min, int max, bool isDebugMode = false)
+	{
+		static auto randomEngine = [isDebugMode]() { return isDebugMode ? std::mt19937{} : std::mt19937(std::random_device{}()); }();
+		auto distribution = std::uniform_int_distribution<int>(min, max);
+		return distribution(randomEngine);
+	}
+}
